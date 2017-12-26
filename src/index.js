191fc36fd56ab7eec56a6f4588d3cf41df0784f7
Christopher Salinas <casjr13@unm.edu>
2017-12-26 11:29:18 -0700

    chore(localStorage): use localStorage on demo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,59 @@
 import './theme.scss';
 
+var storage = window.localStorage;
+var layout = document.querySelector('.layout');
+var layoutChangers = document.getElementsByClassName('layoutChanger');
+var menuToggle = document.getElementById('menutoggle');
+var nightToggle = document.getElementById('nighttoggle');
+
+var updateNightModeClasses = function(e) {
+  var night;
+  if (e) {
+    e.preventDefault();
+    night = !(layout.classList.contains('night'));
+    storage.setItem('night', night);
+  } else {
+    night = storage.getItem('night') === 'true';
+  }
+
+  if (night) {
+    layout.classList.add('night');
+    nightToggle.innerHTML = 'brightness_3'
+  } else {
+    layout.classList.remove('night');
+    nightToggle.innerHTML = 'brightness_7';
+  }
+}
+
+var updateLayout = function(e) {
+  var initialLayout;
+  if (e) {
+    e.preventDefault();
+    initialLayout = e.target.text;
+    storage.setItem('initialLayout', initialLayout);
+  } else {
+    initialLayout = storage.getItem('initialLayout');
+  }
+
+  layout.classList.remove('split-header', 'sidebar');
+  switch (initialLayout) {
+    case 'Split Header':
+      layout.classList.add('split-header');
+      break;
+    case 'Sidebar':
+      layout.classList.add('sidebar');
+      break;
+    default:
+      break;
+  }
+}
+
 window.onload = function() {
-  var layout = document.querySelector('.layout');
-  var menuToggle = document.getElementById('menutoggle');
-  var nightToggle = document.getElementById('nighttoggle');
+  // Get initial layout
+  updateLayout();
+  
+  // Get current mode
+  updateNightModeClasses();
 
   // Setup the menu toggle
   menuToggle.addEventListener('click', function(e) {
@@ -11,14 +61,10 @@ window.onload = function() {
   });
 
   // Setup the night mode toggle
-  nightToggle.addEventListener('click', function(e) {
-    layout.classList.toggle('night');
+  nightToggle.addEventListener('click', updateNightModeClasses);
 
-    if (layout.classList.contains('night')) {
-      nightToggle.innerHTML = 'brightness_3'
-    } else {
-      nightToggle.innerHTML = 'brightness_7';
-    }
-  });
-  
+  // Setup layout changing anchors
+  for (var i = 0; i < layoutChangers.length; i++) {
+    layoutChangers[i].addEventListener('click', updateLayout);
+  }
 };
