59d7723af914be55960c623c23b5a238150b6f2a
Christopher Salinas <casjr13@unm.edu>
2017-12-26 09:05:33 -0700

    feat(buttons): initial implementations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import './theme.scss';
 
 window.onload = function() {
+  var layout = document.querySelector('.layout');
+  var menuToggle = document.getElementById('menutoggle');
+  var nightToggle = document.getElementById('nighttoggle');
+
   // Setup the menu toggle
-  document.getElementById('menutoggle').addEventListener('click', function(e) {
-    document.querySelector('.layout').classList.toggle('show-menu');
+  menuToggle.addEventListener('click', function(e) {
+    layout.classList.toggle('show-menu');
   });
+
+  // Setup the night mode toggle
+  nightToggle.addEventListener('click', function(e) {
+    layout.classList.toggle('night');
+
+    if (layout.classList.contains('night')) {
+      nightToggle.innerHTML = 'brightness_3'
+    } else {
+      nightToggle.innerHTML = 'brightness_7';
+    }
+  });
+  
 };
