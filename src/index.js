24056078257595c810708812e94b8b16a816cacf
Christopher Salinas <casjr13@unm.edu>
2017-12-27 12:03:32 -0700

    feat: more functionality
    I cleaned/refactored the SASS.
    I added a scrolling-top event class for the default nav
    Mobile first SASS

--- src/index.js
+++ src/index.js
@@ -1,70 +1,31 @@
 import './theme.scss';
 
-var storage = window.localStorage;
 var layout = document.querySelector('.layout');
-var layoutChangers = document.getElementsByClassName('layoutChanger');
 var menuToggle = document.getElementById('menutoggle');
-var nightToggle = document.getElementById('nighttoggle');
 
-var updateNightModeClasses = function(e) {
-  var night;
-  if (e) {
-    e.preventDefault();
-    night = !(layout.classList.contains('night'));
-    storage.setItem('night', night);
+// Handle scrolling event
+var position = 0;
+var scrollingUp = false;
+var scrollChange = function() {
+  if (scrollingUp) {
+    layout.classList.add('scrolling-up');
   } else {
-    night = storage.getItem('night') === 'true';
-  }
-
-  if (night) {
-    layout.classList.add('night');
-    nightToggle.innerHTML = 'brightness_3'
-  } else {
-    layout.classList.remove('night');
-    nightToggle.innerHTML = 'brightness_7';
+    layout.classList.remove('scrolling-up');
   }
 }
-
-var updateLayout = function(e) {
-  var initialLayout;
-  if (e) {
-    e.preventDefault();
-    initialLayout = e.target.text;
-    storage.setItem('initialLayout', initialLayout);
-  } else {
-    initialLayout = storage.getItem('initialLayout');
+window.onscroll = function() {
+  var newVal = position <= window.scrollY;
+  if (scrollingUp !== newVal) {
+    scrollChange();
   }
-
-  layout.classList.remove('split-header', 'sidebar');
-  switch (initialLayout) {
-    case 'Split Header':
-      layout.classList.add('split-header');
-      break;
-    case 'Sidebar':
-      layout.classList.add('sidebar');
-      break;
-    default:
-      break;
-  }
+  scrollingUp = newVal;
+  position = window.scrollY;
 }
 
+// Steps to take when the window loads
 window.onload = function() {
-  // Get initial layout
-  updateLayout();
-  
-  // Get current mode
-  updateNightModeClasses();
-
   // Setup the menu toggle
   menuToggle.addEventListener('click', function(e) {
     layout.classList.toggle('show-menu');
   });
-
-  // Setup the night mode toggle
-  nightToggle.addEventListener('click', updateNightModeClasses);
-
-  // Setup layout changing anchors
-  for (var i = 0; i < layoutChangers.length; i++) {
-    layoutChangers[i].addEventListener('click', updateLayout);
-  }
 };
